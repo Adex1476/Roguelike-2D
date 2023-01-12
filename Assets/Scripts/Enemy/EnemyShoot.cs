6bd2f1aa1869c6f1c060a88618ac5edcfd0abfbd
Aitor Cotano <aitor.cotano.7e4@itb.cat>
2023-01-12 19:57:29 +0100

    Enemy2 Death DONE

--- Assets/Scripts/Enemy/EnemyShoot.cs
+++ Assets/Scripts/Enemy/EnemyShoot.cs
@@ -7,12 +7,15 @@ public class EnemyShoot : MonoBehaviour
     [SerializeField] private GameObject _proj;
     [SerializeField] private Transform _projPos;
     [SerializeField] private Animator _anim;
+    [SerializeField] private Animator animator;
+    private bool _isDead;
     private float timer;
+    public int cont;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _isDead = false;
     }
 
     // Update is called once per frame
@@ -31,4 +34,23 @@ public class EnemyShoot : MonoBehaviour
         _anim.SetTrigger("Active");
         Instantiate(_proj, _projPos.position, Quaternion.identity);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            cont += 1;
+            if (cont == 5) { CollisionBehaviour(); }
+        }
+    }
+
+    void CollisionBehaviour()
+    {
+        Destroy(this.GetComponent<CapsuleCollider2D>());
+        _isDead = true;
+        animator.SetTrigger("Death");
+        Invoke("Destroy", 1f);
+    }
+
+    private void Destroy() { Destroy(this.gameObject); }
 }
